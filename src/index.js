1951df89c4ef388ee7f93e06d8f9babfc3d21c7e
dericparra <parradeko@gmail.com>
2021-11-16 19:02:32 -0300

    Chaging the SPA in to a PWA

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
@@ -8,3 +9,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+serviceWorkerRegistration.register()
